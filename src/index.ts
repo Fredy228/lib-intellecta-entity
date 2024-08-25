800efc9781f0f782608cb4603876a47eb82a1382
Fredy228 <smagrovich58@gmail.com>
2024-08-25 21:54:56 +0300

    Add variable ALL ENTITIES

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 //Entity
+import { User, UserDevices } from "./user/user.entity";
+import { Profile } from "./user/proflle.entity";
+import { Moderator } from "./user/admin.entity";
+import { Owner } from "./user/owner.entity";
+import { Teacher } from "./user/teacher.entity";
+import { Student } from "./user/student.entity";
+import { University } from "./university/university.entity";
+import { Subject } from "./university/subject.entity";
+import { Faculty } from "./university/faculty.entity";
+import { Group } from "./group/group.entity";
+import { Icon } from "./icon.entity";
+import { SupportMessage } from "./support-message.entity";
+
 export { User, UserDevices } from "./user/user.entity";
 export { Profile } from "./user/proflle.entity";
 export { Moderator } from "./user/admin.entity";
@@ -11,6 +24,22 @@ export { Faculty } from "./university/faculty.entity";
 export { Group } from "./group/group.entity";
 export { Icon } from "./icon.entity";
 export { SupportMessage } from "./support-message.entity";
+
+export const ALL_ENTITIES = [
+  User,
+  UserDevices,
+  Profile,
+  Moderator,
+  Owner,
+  Teacher,
+  Student,
+  University,
+  Subject,
+  Faculty,
+  Group,
+  Icon,
+  SupportMessage,
+];
 
 //Enum
 export * from "./enum/icon.enum";
